06b2b854fc8dcb35ec3163c1bd03d70d7e99faff
babyshoes <dianecai@gmail.com>
2015-09-08 21:24:01 -0400

    all done

--- js/owner.js
+++ js/owner.js
@@ -1,7 +1,17 @@
-function Owner(){
-  // attributes go here...
+function Owner(name){
+  this.name = name;
+  this.bikes = [];
 }
 
-Owner.prototype.someInstanceMethod = function(){
-  // code goes here...
+Owner.prototype.buildBike = function(name, bikeType, frameColor){
+  var bike = new Bike(name, bikeType, frameColor);
+  this.bikes.push(bike);
+};
+
+Owner.prototype.rideBike = function(bike, miles){
+  for(var i=0; i<this.bikes.length; i++){
+    if(this.bikes[i].name == bike){
+      this.bikes[i].condition = "needs a tune up";
+    }
+  }
 };
